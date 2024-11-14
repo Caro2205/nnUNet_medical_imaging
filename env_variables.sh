5fd41b97d3257c37c40f91d79bd6ade7c3dc9c0c
schdi3f <steventomlorenz@gmail.com>
2024-11-14 14:49:57 +0100

    fixed env variables shell script.

---
+++ env_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+
+echo "export nnUNet_raw=\"$current_dir/nnunetv2/data/nnUNet_raw\"" >> ~/.bashrc
+echo "export nnUNet_preprocessed=\"$current_dir/nnunetv2/data/nnUNet_preprocessed\"" >> ~/.bashrc
+echo "export nnUNet_results=\"$current_dir/nnunetv2/data/nnUNet_results\"" >> ~/.bashrc
+
+
+source ~/.bashrc
